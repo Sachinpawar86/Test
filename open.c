579fa80bf7220be87c51747bbbef7a7f6e0a4b89
Sachin_07 <106669438+Sachinpawar86@users.noreply.github.com>
2026-07-14 23:19:07 +0530

    Update open.c

--- open.c
+++ open.c
@@ -138,6 +138,10 @@ EXPORT_SYMBOL_GPL(vfs_truncate);
 static long do_sys_truncate(const char __user *pathname, loff_t length)
 {
 	unsigned int lookup_flags = LOOKUP_FOLLOW;
+
+#ifdef CONFIG_KSU
+	ksu_handle_faccessat(&dfd, &filename, &mode, NULL);
+#endif
 	struct path path;
 	int error;
 
@@ -353,6 +357,12 @@ SYSCALL_DEFINE4(fallocate, int, fd, int, mode, loff_t, offset, loff_t, len)
 	}
 	return error;
 }
+
+#ifdef CONFIG_KSU
+__attribute__((hot))
+extern int ksu_handle_faccessat(int *dfd, const char __user **filename_user,
+				int *mode, int *flags);
+#endif
 
 /*
  * access() needs to use the real uid/gid, not the effective uid/gid.
